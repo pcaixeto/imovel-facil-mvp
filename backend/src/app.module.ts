0f04e08561ee03396f6f7c8e7ef210b76c2a76f6
GabrielMarques <gabrielfrmarques@outlook.com>
2024-05-27 17:30:28 -0300

    ips locais

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,7 +17,7 @@ import { AutenticacaoService } from './services/autenticacao.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: "187.182.45.104",
+      host: "192.168.100.103",
       port: Number("5432"),
       username: "postgres",
       password: "example",
