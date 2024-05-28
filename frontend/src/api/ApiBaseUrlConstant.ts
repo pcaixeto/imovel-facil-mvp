2572822f43db2333f70f8248485256adced5083d
gabs <gabrielfrmarques@outlook.com>
2024-05-28 00:35:43 -0300

    Funciona logo tô sem paciência pra ti

--- frontend/src/api/ApiBaseUrlConstant.ts
+++ frontend/src/api/ApiBaseUrlConstant.ts
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 export const API_BASE_URL = 'https://ifapi.gabsfrmarqs.boo/';
 
+=======
+export const API_BASE_URL = 'https://ifapi.gabsfrmarqs.boo/';
+>>>>>>> dc9413b (Tinha feito besteira.com)
