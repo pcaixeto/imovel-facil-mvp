e656f9cbbb73b57f6de4c4b4dba5be67b699ab59
gabs <gabrielfrmarques@outlook.com>
2024-05-28 09:40:15 -0300

    Vamo amigo

--- frontend/src/api/ApiBaseUrlConstant.ts
+++ frontend/src/api/ApiBaseUrlConstant.ts
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export const API_BASE_URL = 'https://ifapi.gabsfrmarqs.boo/';
-
-=======
-export const API_BASE_URL = 'https://ifapi.gabsfrmarqs.boo/';
->>>>>>> dc9413b (Tinha feito besteira.com)
